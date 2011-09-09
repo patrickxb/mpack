a06bf96e49ed79c482c33054ae2604079e751e78
Patrick Crosby <patrick@xblabs.com>
2011-09-09 11:01:49 -0500

    formatting

--- array.go
+++ array.go
@@ -1,101 +1,101 @@
 package mpack
 
 import (
-	"bytes"
-	"reflect"
+        "bytes"
+        "reflect"
 )
 
 type Array struct {
-	raw []interface{}
+        raw []interface{}
 }
 
 func NewArray(generic interface{}) *Array {
-	result := new(Array)
-	result.raw = generic.([]interface{})
-	return result
+        result := new(Array)
+        result.raw = generic.([]interface{})
+        return result
 }
 
 func NewEmptyArray() *Array {
-	result := new(Array)
-	result.raw = make([]interface{}, 0)
-	return result
+        result := new(Array)
+        result.raw = make([]interface{}, 0)
+        return result
 }
 
 func (a Array) Len() int {
-	return len(a.raw)
+        return len(a.raw)
 }
 
 func (a Array) Raw() []interface{} {
-	return a.raw
+        return a.raw
 }
 
 func (a Array) Item(index int) interface{} {
-	return a.raw[index]
+        return a.raw[index]
 }
 
 func (a Array) IntItem(index int) int64 {
-	v := reflect.ValueOf(a.raw[index])
-	if isInt(v) {
-		return v.Int()
-	}
-	if isUint(v) {
-		return int64(v.Uint())
-	}
-	return 0
+        v := reflect.ValueOf(a.raw[index])
+        if isInt(v) {
+                return v.Int()
+        }
+        if isUint(v) {
+                return int64(v.Uint())
+        }
+        return 0
 }
 
 func (a Array) UintItem(index int) uint64 {
-	v := reflect.ValueOf(a.raw[index])
-	if isUint(v) {
-		return v.Uint()
-	}
-	if isInt(v) {
-		return uint64(v.Int())
-	}
-	return 0
+        v := reflect.ValueOf(a.raw[index])
+        if isUint(v) {
+                return v.Uint()
+        }
+        if isInt(v) {
+                return uint64(v.Int())
+        }
+        return 0
 }
 
 func (a Array) Uint32Item(index int) uint32 {
-	v := reflect.ValueOf(a.raw[index])
-	if isUint(v) {
-		return uint32(v.Uint())
-	}
-	if isInt(v) {
-		return uint32(v.Int())
-	}
-	return 0
+        v := reflect.ValueOf(a.raw[index])
+        if isUint(v) {
+                return uint32(v.Uint())
+        }
+        if isInt(v) {
+                return uint32(v.Int())
+        }
+        return 0
 }
 
 func (a Array) StringItem(index int) string {
-	return string(a.raw[index].([]uint8))
+        return string(a.raw[index].([]uint8))
 }
 
 func (a Array) BufferItem(index int) *bytes.Buffer {
-	return bytes.NewBuffer(a.raw[index].([]byte))
+        return bytes.NewBuffer(a.raw[index].([]byte))
 }
 
 func (a Array) ArrayItem(index int) *Array {
-	return NewArray(a.raw[index])
+        return NewArray(a.raw[index])
 }
 
 func (a Array) MapItem(index int) *Map {
-	return NewMap(a.raw[index])
+        return NewMap(a.raw[index])
 }
 
 func (a Array) FloatItem(index int) float64 {
-	v := reflect.ValueOf(a.raw[index])
-	if isFloat(v) {
-		return v.Float()
-	}
-	if isInt(v) {
-		return float64(v.Int())
-	}
-	if isUint(v) {
-		return float64(v.Uint())
-	}
-	return 0.0
+        v := reflect.ValueOf(a.raw[index])
+        if isFloat(v) {
+                return v.Float()
+        }
+        if isInt(v) {
+                return float64(v.Int())
+        }
+        if isUint(v) {
+                return float64(v.Uint())
+        }
+        return 0.0
 }
 
 func (a *Array) Append(item interface{}) {
-	a.raw = append(a.raw, item)
+        a.raw = append(a.raw, item)
 }
--- rpc_old.go
+++ rpc_old.go
@@ -3,274 +3,274 @@ package mpack
 /* a lot of this is borrowed from golang standard rpc package */
 
 import (
-	"log"
-	"net"
-	"reflect"
-	"os"
-	"strings"
-	"time"
-	"bytes"
+        "log"
+        "net"
+        "reflect"
+        "os"
+        "strings"
+        "time"
+        "bytes"
 )
 
 type methodType struct {
-	method reflect.Method
+        method reflect.Method
 }
 
 type service struct {
-	name    string
-	rcvr    reflect.Value
-	kind    reflect.Type
-	methods map[string]*methodType
+        name    string
+        rcvr    reflect.Value
+        kind    reflect.Type
+        methods map[string]*methodType
 }
 
 type Server struct {
-	Host       string
-	serviceMap map[string]*service
+        Host       string
+        serviceMap map[string]*service
 }
 
 func NewServer(host string) *Server {
-	result := new(Server)
-	result.Host = host
-	return result
+        result := new(Server)
+        result.Host = host
+        return result
 }
 
 func (server *Server) Register(rcvr interface{}) os.Error {
-	if server.serviceMap == nil {
-		server.serviceMap = make(map[string]*service)
-	}
-	s := new(service)
-	s.kind = reflect.TypeOf(rcvr)
-	s.rcvr = reflect.ValueOf(rcvr)
-	sname := reflect.Indirect(s.rcvr).Type().Name()
+        if server.serviceMap == nil {
+                server.serviceMap = make(map[string]*service)
+        }
+        s := new(service)
+        s.kind = reflect.TypeOf(rcvr)
+        s.rcvr = reflect.ValueOf(rcvr)
+        sname := reflect.Indirect(s.rcvr).Type().Name()
 
-	// XXX there's some stuff in rpc/server.go that I'm skipping
+        // XXX there's some stuff in rpc/server.go that I'm skipping
 
-	if _, present := server.serviceMap[sname]; present {
-		return os.NewError("rpc service is already defined: " + sname)
-	}
-	s.name = sname
-	s.methods = make(map[string]*methodType)
+        if _, present := server.serviceMap[sname]; present {
+                return os.NewError("rpc service is already defined: " + sname)
+        }
+        s.name = sname
+        s.methods = make(map[string]*methodType)
 
-	for m := 0; m < s.kind.NumMethod(); m++ {
-		method := s.kind.Method(m)
-		// mtype := method.Type
-		mname := method.Name
+        for m := 0; m < s.kind.NumMethod(); m++ {
+                method := s.kind.Method(m)
+                // mtype := method.Type
+                mname := method.Name
 
-		s.methods[mname] = &methodType{method: method}
-		log.Println("registered method", mname)
-	}
+                s.methods[mname] = &methodType{method: method}
+                log.Println("registered method", mname)
+        }
 
-	if len(s.methods) == 0 {
-		return os.NewError("register type " + sname + " has no exported methods")
-	}
+        if len(s.methods) == 0 {
+                return os.NewError("register type " + sname + " has no exported methods")
+        }
 
-	server.serviceMap[s.name] = s
-	log.Printf("service map: %s", server.serviceMap)
-	return nil
+        server.serviceMap[s.name] = s
+        log.Printf("service map: %s", server.serviceMap)
+        return nil
 }
 
 func (server *Server) HasMethod(serviceName, methodName string) bool {
-	if _, present := server.serviceMap[serviceName]; present == false {
-		return false
-	}
-	srvc, _ := server.serviceMap[serviceName]
-	if _, present := srvc.methods[methodName]; present == false {
-		return false
-	}
-	return true
+        if _, present := server.serviceMap[serviceName]; present == false {
+                return false
+        }
+        srvc, _ := server.serviceMap[serviceName]
+        if _, present := srvc.methods[methodName]; present == false {
+                return false
+        }
+        return true
 }
 
 func (server *Server) CallMethod(serviceName, methodName string, args interface{}) (interface{}, os.Error) {
-	srvc, present := server.serviceMap[serviceName]
-	if present == false {
-		return nil, os.NewError("no service found: " + serviceName)
-	}
-	method, present := srvc.methods[methodName]
-	if present == false {
-		return nil, os.NewError("no method in " + serviceName + " found named " + methodName)
-	}
-	//	log.Printf("method: %s", method)
-	function := method.method.Func
-	results := function.Call([]reflect.Value{srvc.rcvr, reflect.ValueOf(args)})
-	//	log.Printf("result[0]: %s", results[0].Interface())
-	//	log.Printf("result[1]: %s", results[1].Interface())
-	err := results[1].Interface()
-	if err != nil {
-		return nil, err.(os.Error)
-	}
-	return results[0].Interface(), nil
+        srvc, present := server.serviceMap[serviceName]
+        if present == false {
+                return nil, os.NewError("no service found: " + serviceName)
+        }
+        method, present := srvc.methods[methodName]
+        if present == false {
+                return nil, os.NewError("no method in " + serviceName + " found named " + methodName)
+        }
+        //	log.Printf("method: %s", method)
+        function := method.method.Func
+        results := function.Call([]reflect.Value{srvc.rcvr, reflect.ValueOf(args)})
+        //	log.Printf("result[0]: %s", results[0].Interface())
+        //	log.Printf("result[1]: %s", results[1].Interface())
+        err := results[1].Interface()
+        if err != nil {
+                return nil, err.(os.Error)
+        }
+        return results[0].Interface(), nil
 }
 
 func (server *Server) replySuccess(conn net.Conn, msgid uint32, result interface{}) os.Error {
-	response := make([]interface{}, 4)
-	response[0] = rpc_response
-	response[1] = msgid
-	response[2] = nil
-	response[3] = result
-	b := new(bytes.Buffer)
-	_, err := Pack(b, response)
-	if err != nil {
-		return err
-	}
-	_, err = conn.Write(b.Bytes())
-	return err
+        response := make([]interface{}, 4)
+        response[0] = rpc_response
+        response[1] = msgid
+        response[2] = nil
+        response[3] = result
+        b := new(bytes.Buffer)
+        _, err := Pack(b, response)
+        if err != nil {
+                return err
+        }
+        _, err = conn.Write(b.Bytes())
+        return err
 }
 
 func (server *Server) replyError(conn net.Conn, msgid uint32, errmsg string) os.Error {
-	response := make([]interface{}, 4)
-	response[0] = rpc_response
-	response[1] = msgid
-	response[2] = errmsg
-	response[3] = nil
-	b := new(bytes.Buffer)
-	_, err := Pack(b, response)
-	if err != nil {
-		return err
-	}
-	_, err = conn.Write(b.Bytes())
-	return err
+        response := make([]interface{}, 4)
+        response[0] = rpc_response
+        response[1] = msgid
+        response[2] = errmsg
+        response[3] = nil
+        b := new(bytes.Buffer)
+        _, err := Pack(b, response)
+        if err != nil {
+                return err
+        }
+        _, err = conn.Write(b.Bytes())
+        return err
 }
 
 func (server *Server) handleRPC(conn net.Conn) {
-	for {
-		startTime := time.Nanoseconds()
-		rpc, _, err := Unpack(conn)
-		if err != nil {
-			log.Printf("read error: %s", err)
-			return
-		}
+        for {
+                startTime := time.Nanoseconds()
+                rpc, _, err := Unpack(conn)
+                if err != nil {
+                        log.Printf("read error: %s", err)
+                        return
+                }
 
-		// log.Printf("rpc (%d bytes): %s", bytesRead, rpc)
-		args := rpc.([]interface{})
-		if args[0] == rpc_request {
-			mv := reflect.ValueOf(args[1])
-			msgid := uint32(mv.Uint())
-			procedure := string(args[2].([]uint8))
-			procedureArgs := args[3]
-			//			log.Printf("rpc request: msgid=%d, proc=%s, args=%s", msgid, procedure, procedureArgs)
-			procElts := strings.Split(procedure, "/")
-			if len(procElts) != 2 {
-				log.Printf("invalid procedure: %s\n", procedure)
-				server.replyError(conn, msgid, "invalid procedure: "+procedure)
-				continue
-			}
+                // log.Printf("rpc (%d bytes): %s", bytesRead, rpc)
+                args := rpc.([]interface{})
+                if args[0] == rpc_request {
+                        mv := reflect.ValueOf(args[1])
+                        msgid := uint32(mv.Uint())
+                        procedure := string(args[2].([]uint8))
+                        procedureArgs := args[3]
+                        //			log.Printf("rpc request: msgid=%d, proc=%s, args=%s", msgid, procedure, procedureArgs)
+                        procElts := strings.Split(procedure, "/")
+                        if len(procElts) != 2 {
+                                log.Printf("invalid procedure: %s\n", procedure)
+                                server.replyError(conn, msgid, "invalid procedure: "+procedure)
+                                continue
+                        }
 
-			if server.HasMethod(procElts[0], procElts[1]) == false {
-				log.Printf("err:  no procedure: %s\n", procedure)
-				server.replyError(conn, msgid, "no procedure: "+procedure)
-				continue
-			}
-			result, err := server.CallMethod(procElts[0], procElts[1], procedureArgs)
-			if err != nil {
-				log.Printf("error calling method: %s\n", err)
-				server.replyError(conn, msgid, "error calling method: "+err.String())
-				continue
-			}
+                        if server.HasMethod(procElts[0], procElts[1]) == false {
+                                log.Printf("err:  no procedure: %s\n", procedure)
+                                server.replyError(conn, msgid, "no procedure: "+procedure)
+                                continue
+                        }
+                        result, err := server.CallMethod(procElts[0], procElts[1], procedureArgs)
+                        if err != nil {
+                                log.Printf("error calling method: %s\n", err)
+                                server.replyError(conn, msgid, "error calling method: "+err.String())
+                                continue
+                        }
 
-			err = server.replySuccess(conn, msgid, result)
-			if err != nil {
-				log.Printf("error replying: %s\n", err)
-			}
-			endTime := time.Nanoseconds()
-			msecs := (float64)(endTime-startTime) / 1000000
-			log.Printf("%s request time: %.3fms", procedure, msecs)
-		}
-	}
+                        err = server.replySuccess(conn, msgid, result)
+                        if err != nil {
+                                log.Printf("error replying: %s\n", err)
+                        }
+                        endTime := time.Nanoseconds()
+                        msecs := (float64)(endTime-startTime) / 1000000
+                        log.Printf("%s request time: %.3fms", procedure, msecs)
+                }
+        }
 }
 
 func (server *Server) Run() {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", server.Host)
-	if err != nil {
-		log.Printf("error resolving address %s: %s", server.Host, err)
-		return
-	}
+        tcpAddr, err := net.ResolveTCPAddr("tcp", server.Host)
+        if err != nil {
+                log.Printf("error resolving address %s: %s", server.Host, err)
+                return
+        }
 
-	listener, err := net.Listen(tcpAddr.Network(), tcpAddr.String())
-	if err != nil {
-		log.Printf("error listening: %s", err)
-		return
-	}
+        listener, err := net.Listen(tcpAddr.Network(), tcpAddr.String())
+        if err != nil {
+                log.Printf("error listening: %s", err)
+                return
+        }
 
-	defer listener.Close()
-	log.Printf("Starting server run loop, listening on %s.", server.Host)
+        defer listener.Close()
+        log.Printf("Starting server run loop, listening on %s.", server.Host)
 
-	for {
-		log.Printf("waiting for connections")
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("client error: %s", err)
-			continue
-		}
+        for {
+                log.Printf("waiting for connections")
+                conn, err := listener.Accept()
+                if err != nil {
+                        log.Printf("client error: %s", err)
+                        continue
+                }
 
-		log.Printf("connection established: %s", conn)
-		go server.handleRPC(conn)
-	}
+                log.Printf("connection established: %s", conn)
+                go server.handleRPC(conn)
+        }
 
 }
 
 type Client struct {
-	Host       string
-	Connection *net.TCPConn
-	Connected  bool
-	msgid      int64
+        Host       string
+        Connection *net.TCPConn
+        Connected  bool
+        msgid      int64
 }
 
 func NewClient(host string) *Client {
-	result := new(Client)
-	result.Host = host
-	result.Connected = false
-	result.msgid = 0
-	return result
+        result := new(Client)
+        result.Host = host
+        result.Connected = false
+        result.msgid = 0
+        return result
 }
 
 func (c *Client) Connect() os.Error {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", c.Host)
-	if err != nil {
-		return err
-	}
-	c.Connection, err = net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		return err
-	}
-	c.Connected = true
-	return nil
+        tcpAddr, err := net.ResolveTCPAddr("tcp", c.Host)
+        if err != nil {
+                return err
+        }
+        c.Connection, err = net.DialTCP("tcp", nil, tcpAddr)
+        if err != nil {
+                return err
+        }
+        c.Connected = true
+        return nil
 }
 
 func (c *Client) Close() os.Error {
-	err := c.Close()
-	if err != nil {
-		return err
-	}
-	c.Connected = false
-	return nil
+        err := c.Close()
+        if err != nil {
+                return err
+        }
+        c.Connected = false
+        return nil
 }
 
 // pass in a channel, return the data on a channel?
 func (c *Client) Call(method string, args interface{}) (interface{}, os.Error) {
-	params := make([]interface{}, 4)
-	params[0] = rpc_request
-	params[1] = c.msgid
-	params[2] = method
-	params[3] = args
-	if c.Connected == false {
-		c.Connect()
-	}
+        params := make([]interface{}, 4)
+        params[0] = rpc_request
+        params[1] = c.msgid
+        params[2] = method
+        params[3] = args
+        if c.Connected == false {
+                c.Connect()
+        }
 
-	b := new(bytes.Buffer)
-	_, err := Pack(b, params)
-	if err != nil {
-		return nil, err
-	}
-	_, err = c.Connection.Write(b.Bytes())
-	if err != nil {
-		return nil, err
-	}
+        b := new(bytes.Buffer)
+        _, err := Pack(b, params)
+        if err != nil {
+                return nil, err
+        }
+        _, err = c.Connection.Write(b.Bytes())
+        if err != nil {
+                return nil, err
+        }
 
-	rpc, _, err := Unpack(c.Connection)
-	if err != nil {
-		log.Printf("read error: %s", err)
-		return nil, err
-	}
-	c.msgid += 1
-	return rpc, err
+        rpc, _, err := Unpack(c.Connection)
+        if err != nil {
+                log.Printf("read error: %s", err)
+                return nil, err
+        }
+        c.msgid += 1
+        return rpc, err
 }
